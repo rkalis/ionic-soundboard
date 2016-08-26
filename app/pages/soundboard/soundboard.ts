9721a007d2ccb0677ad5951b64967070c91c22fc
Rosco Kalis <roscokalis@gmail.com>
2016-08-26 19:46:16 +0200

    Tidying up and commenting

--- app/pages/soundboard/soundboard.ts
+++ app/pages/soundboard/soundboard.ts
@@ -15,6 +15,7 @@ export class SoundboardPage {
 
   /* Icon Colours */
   /* EDIT THESE */
+  colour: string = "black";
   colours: Array<string> = [
     "red",
     "blue",
@@ -22,18 +23,23 @@ export class SoundboardPage {
     "purple",
     "cyan"
   ];
-  colour: string = "black";
 
   sounds: any = [];
   media: any = null;
+
   constructor(public http: Http) {
     this.http.get(this.base_url + this.sounds_url)
       .subscribe(
         data => {
+          /* Create a webpage out of the data from the http.get request */
           let content: string = data.text();
           let doc: any = document.createElement("html");
           doc.innerHTML = content;
+
+          /* Extract all "a" tags from it */
           let links: any = doc.getElementsByTagName("a");
+
+          /* Loop through them, saving their title and sound file */
           for(let link of links) {
             this.sounds.push({
               title: link.innerHTML,
@@ -46,22 +52,24 @@ export class SoundboardPage {
        );
   }
 
+  /* returns an ngStyle-compliant object containing either a random colour
+   * or a specific colour, depending on set variables
+   */
   colourStyle() {
     if(this.randomColours) {
-      let colour: string = this.colours[Math.floor(
-                                        Math.random() * this.colours.length)];
+      let colour: string = this.colours[Math.floor(Math.random() * this.colours.length)];
       return {color: colour};
     }
     return {color: this.colour};
   }
 
+  /* Plays a sound, pausing other playing sounds if necessary */
   play(sound) {
     console.log(sound)
     if(this.media) {
       this.media.pause();
     }
-    this.media = new Audio();
-    this.media.src = sound.file;
+    this.media = new Audio(sound.file);
     this.media.load();
     this.media.play();
   }
