dbd09f8d34f1020d40814eb3efb5b46fd3810c19
Rosco Kalis <roscokalis@gmail.com>
2017-08-04 19:18:04 +0200

    Fully integrated CacheService and Cordova Media into the soundboard

--- src/pages/soundboard/soundboard.ts
+++ src/pages/soundboard/soundboard.ts
@@ -3,6 +3,7 @@ import { Http } from '@angular/http';
 import { FavouritesData } from '../../providers/favouritesdata';
 import { CacheService } from '../../providers/cache.service';
 import { Media } from '@ionic-native/media';
+import { NgZone } from '@angular/core';
 
 
 @Component({
@@ -11,157 +12,161 @@ import { Media } from '@ionic-native/media';
 export class SoundboardPage {
 
   /* EDIT THESE */
-  title: string = "Ionic Soundboard";
-  base_url: string = "http://kalis.me";
-  sounds_file: string = "/sounds.json"
+  title = 'Ionic Soundboard';
+  base_url = 'http://kalis.me';
+  sounds_file = '/sounds.json';
 
   sounds: any = [];
   media: any = null;
 
-  constructor(public http: Http, public favouritesData: FavouritesData, public cacheService: CacheService, public mediaService: Media) {
+  constructor(private http: Http, public favouritesData: FavouritesData, private mediaService: Media,
+              private cacheService: CacheService, private zone: NgZone) {
     this.cacheService.ready().then(() => {
       this.cacheService.getCache().forEach(cachedSound => {
         cachedSound.isPlaying = false;
         this.sounds.push(cachedSound);
       });
+      this.getRemoteSounds();
+    }).catch(error => console.log(error));
+  }
 
-      this.http.get(this.base_url + this.sounds_file)
-      .subscribe(
-        data => {
-          /* Loop through data
-           * Format:
-           * [
-           *   {
-           *     "title": "title",
-           *     "file": "file"
-           *   }
-           * ]
+  /* Gets all sounds found at this.base_url + this.sounds_file */
+  getRemoteSounds() {
+    this.http.get(this.base_url + this.sounds_file)
+      .subscribe(data => {
+        /* Loop through data
+          * Format:
+          * [
+          *   {
+          *     "title": "title",
+          *     "file": "file"
+          *   }
+          * ]
+          */
+        data.json().forEach(sound => {
+          /* Example: http://website.com/soundfile.mp3 */
+          if (!sound.file.startsWith('http')) {
+            if (!sound.file.startsWith('/')) {
+              sound.file = '/' + sound.file;
+            }
+            /* Example: (/)soundfile.mp3 -> http://website.com/soundfile.mp3 */
+            sound.file = this.base_url + sound.file;
+          }
+
+          /* If the file is not already in the cache or it is, but outdated,
+           * only then does the remote sound get added to the list
            */
-          data.json().forEach(sound => {
-            if (!sound.file.startsWith("http")) {
-              if (!sound.file.startsWith("/")) {
-                sound.file = "/" + sound.file;
-              }
-              sound.file = this.base_url + sound.file;
-            }
-            if (!this.cacheService.hasInCache(sound)) {
-              this.sounds.push({
-                title: sound.title,
-                src: sound.file,
-                isPlaying: false
-              })
-            }
-          });
-        },
-        err => console.error('There was an error: ' + err),
-        () => console.log(this.sounds)
-       );
-    });
+          if (!this.cacheService.hasInCache(sound) ||
+              this.cacheService.isOutdated(sound)) {
+            this.sounds.push({
+              title: sound.title,
+              src: sound.file,
+              isPlaying: false
+            });
+          }
+        });
+      },
+      error => console.error(error),
+      () => console.log(this.sounds)
+      );
   }
 
   /* Plays a sound, pausing other playing sounds if necessary */
   play(sound) {
-    console.log(sound);
-    this.cacheService.addToCache(sound);
+    this.stopPlayback();
 
-    if (this.media) {
-      this.stop(sound);
+    /* Plays with Cordova Audio if available, falls back on Web Audio */
+    if (window.hasOwnProperty('cordova') && window.hasOwnProperty('Media')) {
+      this.cache(sound).then(() => this.playWithCordovaAudio(sound));
+    } else {
+      this.playWithWebAudio(sound);
     }
-
-    this.media = this.mediaService.create(sound.src);
-    this.media.statusCallback = status => {
-      console.log(status);
-      if (status == 1 || status == 2) {
-        sound.isPlaying = true;
-      } else if (status == 3 || status == 4) {
-        sound.isPlaying = false;
-      }
-    }
-    this.media.play();
-    // this.media.onStatusUpdate.subscribe(status => {
-    //   console.log(status);
-    //   if (status == this.mediaService.create("test").statusCallback || status == 2) {
-    //     sound.isPlaying = true;
-    //   } else if (status == 3 || status == 4) {
-    //     sound.isPlaying = false;
-    //   }
-    // });
-    // console.log(this.media);
-    // this.media.play();
-
-    // if (this.cacheService.hasInCache(sound)) {
-    //   this.playCachedSound(sound);
-    // } else {
-    //   this.cacheService.addToCache(sound);
-    //   this.playRemoteSound(sound);
-    // }
   }
 
-  playRemoteSound(sound) {
+  playWithWebAudio(sound) {
     this.media = new Audio(sound.file);
 
-    /* Adding event listeners to update the sounds isPlaying attribute accordingly */
+    /* Adding event listeners to update the sound's isPlaying attribute accordingly */
     this.media.onended = () => {
       sound.isPlaying = false;
-    }
+    };
     this.media.onpause = () => {
       sound.isPlaying = false;
-    }
+    };
     this.media.onplay = () => {
       sound.isPlaying = true;
-    }
+    };
 
     this.media.load();
     this.media.play();
   }
 
-  playCachedSound(sound) {
-    let cachedSound = this.cacheService.getFromCache(sound);
-    console.log(cachedSound);
-    this.media = this.mediaService.create(cachedSound.file);
-    this.media.onStatusUpdate.subscribe(status => {
-      console.log(status);
-      if (status == 1) {
-        sound.isPlaying = true;
-      } else if (status == 3 || status == 4) {
-        sound.isPlaying = false;
-      }
-    });
-    console.log(this.media);
+  playWithCordovaAudio(sound) {
+    this.media = this.mediaService.create(sound.src);
+
+    /* Adding status callback to update the sound's isPlaying attribute accordingly */
+    this.media.statusCallback = status => {
+      this.zone.run(() => {
+        switch (status) {
+          case this.mediaService.MEDIA_RUNNING:
+            sound.isPlaying = true;
+            break;
+          case this.mediaService.MEDIA_PAUSED:
+            sound.isPlaying = false;
+            break;
+          case this.mediaService.MEDIA_STOPPED:
+            sound.isPlaying = false;
+            break;
+        }
+      });
+    };
+
     this.media.play();
   }
 
   /* Stops the playback of the sound */
-  stop(sound) {
-    /* Stop playback, unsubscribe from Observables,
-     * release the audio, and set it to null
-     */
+  stopPlayback() {
     if (this.media) {
       this.media.stop();
       if (this.media.release) {
-        this.media.statusCallback = null;
         this.media.release();
       }
-      this.media = null;
     }
+  }
+
+  /* Caches a given sound */
+  cache(sound) {
+    /* Adds a sound to the cache, then updates its attributes to reflect its new status */
+    return this.cacheService.addToCache(sound)
+      .then(cachedSound => {
+        sound.src = cachedSound.src;
+        sound.remoteSrc = cachedSound.remoteSrc;
+        sound.cacheDate = cachedSound.cache;
+      }).catch(error => console.log(error));
+  }
+
+  /* Clears the entire cache, and reloads all remote sounds */
+  clearCacheAndReload() {
+    this.cacheService.clearCache()
+      .then(() => {
+        this.sounds = [];
+        this.getRemoteSounds();
+      }).catch(error => console.log(error));
   }
 
   /* Toggle a sound as favourite */
   toggleFavourite(sound) {
     this.favouritesData.toggleFavourite(sound);
-    console.log(this.favouritesData.getAllFavourites());
   }
 
   /* Lists all favourited sounds */
   listFavouriteSounds() {
-    let allFavourites = this.favouritesData.getAllFavourites();
-    return allFavourites;
+    return this.sounds.filter(sound => this.favouritesData.hasFavourite(sound));
   }
 
   /* Lists all sounds not marked as favourite */
   listRegularSounds() {
-    let regularSounds = this.sounds.filter(sound => !this.favouritesData.hasFavourite(sound));
-    return regularSounds;
+    return this.sounds.filter(sound => !this.favouritesData.hasFavourite(sound));
   }
 
   /* List all sounds, favourites first */
