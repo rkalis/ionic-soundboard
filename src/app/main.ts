563e0634bfe433d5186983f65d1a24468c9f3aa9
Rosco Kalis <roscokalis@gmail.com>
2017-08-04 19:17:06 +0200

    Updated npm packages and tslint config

--- src/app/main.ts
+++ src/app/main.ts
@@ -2,8 +2,4 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
 
-if (window['cordova']) {
-  document.addEventListener('deviceready', () => platformBrowserDynamic().bootstrapModule(AppModule));
-} else {
-  platformBrowserDynamic().bootstrapModule(AppModule);
-}
+platformBrowserDynamic().bootstrapModule(AppModule);
